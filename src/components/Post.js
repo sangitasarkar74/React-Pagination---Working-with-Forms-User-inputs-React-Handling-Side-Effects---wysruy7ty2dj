36cd1b6c2a05ee0183a722b11c186952f561a7b9
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-11 16:16:04 +0530

    updated changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,11 @@
-import React from 'react'
-const Post = () => {
+import React from "react";
+const Post = ({ ele }) => {
     return (
         <div className="post">
-
+            <h3>{ele.title}</h3>
+            <p>{ele.body}</p>
         </div>
-    )
-}
+    );
+};
 
-export { Post }
+export { Post };
